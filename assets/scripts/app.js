e82c7720e405491e95df6f4e5759aa17a15f4bf5
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-10-29 23:29:55 -0700

    Fix issue with roles not appearing in drop down area of form.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,82 +1,85 @@
 // Your code here!
 const apiEndpoint= 'https://galvanize-student-apis.herokuapp.com/gpersonnel';
-const roleChoice = document.querySelector('#role');
-const imageChoice = document.querySelector('.role-preview');
-const saveButton = document.querySelector('.save');
-let roles;
 
 // * Dynamically generate the option list of roles by making a GET request to the following API endpoint:
 // ``` https://galvanize-student-apis.herokuapp.com/gpersonnel/roles```
 document.addEventListener('DOMContentLoaded', function(event) {
+
   axios.get(`${apiEndpoint}/roles`)
     .then(result => {
-      roles = result.data;
 
-      generateRoles(roles);
-      convertFormDataToObj(roles);
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}.`);
-    })
-})
+      //call addRolesToOptions function to dynamicaly create options listed in the drop down part of the Form
+      let rolesArr = result.data;
+      addRolesToOptions(rolesArr);
 
-roleChoice.addEventListener('change', function(event) => {
-  changeImageDisplayed(roles);
-})
+
+      let role = document.querySelector('#role');
+      let options = document.querySelectorAll('option')
+      let preview = document.querySelector('.role-preview');
+
+      role.addEventListener('change', function(event) {
+        rolesArr.forEach(role => {
+          if (event.target.value === role.title) {
+            preview.src = role.img
+          }
+        })
+      })
+    })
+
+    const saveBtn = document.querySelector('.save')
+
+    saveBtn.addEventListener('click', function(event) {
+      event.preventDefault;
+
+      let userObj = convertFormDataToObj();
+
+      axios.post(`${apiEndpoint}/users`, userObj)
+        .then(result => {
+          if (result.status === 201) {
+            message.innerHTML = result.data.message;
+
+            const fadeImage = () => {
+              $('.save-status').fadeOut(500);
+              setTimeout(fadeImage, 2000);
+            }
+          }
+        })
+        .catch((err) => {
+          console.log(`Error: ${err}.`)
+        })
+    })
+}), false);
 
 // * Whenever the save button is clicked, *the default behavior should be prevented*, and a POST request should be made to the following API endpoint:
 //     ```
 //     https://galvanize-student-apis.herokuapp.com/gpersonnel/users
 //     ```
-saveButton.addEventListener('click', function(event) => {
-  event.preventDefault();
-  let user = convertFormDataToObj();
-
-  if (!user) {
-    let message = document.querySelector('.save-status');
-    message.innerHTML = 'Unable to locate user.';
-    message.style.display = "block";
-  }
-
-  // * When the result of the `POST` from the save button returns, you should display the resulting `message` in a paragraph with the `.save-status` class
-  // * You will need to `show` the `.save-status` paragraph after setting the message.
-  axios.post(`${api}/users`, user)
-    .then(result => {
-      let message = document.querySelector('.save-status')
-      if (result.data.message === 'Success!') {
-        message.innerHTML = result.data.message;
-        message.style.display = block;
-      }
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}.`)
-    })
-})
+// * When the result of the `POST` from the save button returns, you should display the resulting `message` in a paragraph with the `.save-status` class
+// * You will need to `show` the `.save-status` paragraph after setting the message.
 
 //uses result of GET request made to https://galvanize-student-apis.herokuapp.com/gpersonnel/roles to dynamically create the items listed in the drop down menu of the form
-function generateRoles(roles) {
+function addRolesToOptions(roles) {
   for (let i=0; i < roles.length; i++) {
-    let role = roles[i].title;
-    let image = roles[i].img;
-    let option = document.createElement('option');
+    let options = document.querySelectorAll('option')
+    let optionElem = document.createElement('option');
+    optionElem.value = `${roles[i].title}`;
+    optionElem.innerHTML = `${roles[i].title}`;
 
-    option.value = `${role}`;
-    option.text = `${role}`;
-    roleChoice.appendChild(option);
+    options.appendChild(optionElem);
   }
 }
 
 // * Whenever the role is changed, the image should be updated with the `img` of that role.
-function changeImageDisplayed(roles) {
-  let roleChosen = roleChoice.options[roleChoice.selectedIndex].text;
-
-  for (let i=0; i <roles.length; i++) {
-    if (roleChosen == roles[i].title) {
-      imageChoice.src = roles[i].img;
-    }
-  }
-  return;
-}
+// function updateImageDisplayed(roleChoice) {
+//   let previewImageURL = document.querySelector('.role-preview')
+//
+//   for (let i=0; i <roles.length; i++) {
+//     if (roleChosen == roles[i].title) {
+//       imageChoice.src = roles[i].img;
+//     }
+//   }
+//   return;
+// }
 
 
 // * The format of the body for the POST request should be:
@@ -88,21 +91,18 @@ function changeImageDisplayed(roles) {
 //   }
 //   ```
 function convertFormDataToObj() {
-  let firstName = document.querySelector('#first_name').value;
-  let lastName = document.querySelector('#last_name').value;
-  let role = document.querySelector('#role').value;
+  const fName = document.querySelector('#first_name').value;
+  const lName = document.querySelector('#last_name').value;
+  const role = document.querySelector('#role').value;
+  const message = document.querySelector('.save-status');
 
-  if (firstName.length === 0  || lastName.length === 0) {
+  if (fName.length === 0  || lName.length === 0) {
     return;
   }
 
   return {
-    firstName: firstName,
-    lastName: lastName,
-    role: role
+    firstName: `${fName}`,
+    lastName: `${lName}`,
+    role: `${role}`
   };
 }
-
-
-
-// * No key or value in the POST body can be empty and the role must match one of the roles returned from the initial API call.
