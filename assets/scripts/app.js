b1a3f573b18c65cc51ee12eb7d1c629bd3b6b989
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-10-29 18:00:38 -0700

    Finish writing all of the items listed in the readme except for how message returned by POST request is displayed.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,22 +6,54 @@ const saveButton = document.querySelector('.save');
 let roles;
 
 // * Dynamically generate the option list of roles by making a GET request to the following API endpoint:
- // ``` https://galvanize-student-apis.herokuapp.com/gpersonnel/roles
- // ```
-
+// ``` https://galvanize-student-apis.herokuapp.com/gpersonnel/roles```
 document.addEventListener('DOMContentLoaded', function(event) {
   axios.get(`${apiEndpoint}/roles`)
     .then(result => {
       roles = result.data;
 
       generateRoles(roles);
-
+      convertFormDataToObj(roles);
     })
     .catch((err) => {
       console.log(`Error: ${err}.`);
     })
 })
 
+roleChoice.addEventListener('change', function(event) => {
+  changeImageDisplayed(roles);
+})
+
+// * Whenever the save button is clicked, *the default behavior should be prevented*, and a POST request should be made to the following API endpoint:
+//     ```
+//     https://galvanize-student-apis.herokuapp.com/gpersonnel/users
+//     ```
+saveButton.addEventListener('click', function(event) => {
+  event.preventDefault();
+  let user = convertFormDataToObj();
+
+  if (!user) {
+    let message = document.querySelector('.save-status');
+    message.innerHTML = 'Unable to locate user.';
+    message.style.display = "block";
+  }
+
+  // * When the result of the `POST` from the save button returns, you should display the resulting `message` in a paragraph with the `.save-status` class
+  // * You will need to `show` the `.save-status` paragraph after setting the message.
+  axios.post(`${api}/users`, user)
+    .then(result => {
+      let message = document.querySelector('.save-status')
+      if (result.data.message === 'Success!') {
+        message.innerHTML = result.data.message;
+        message.style.display = block;
+      }
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}.`)
+    })
+})
+
+//uses result of GET request made to https://galvanize-student-apis.herokuapp.com/gpersonnel/roles to dynamically create the items listed in the drop down menu of the form 
 function generateRoles(roles) {
   for (let i=0; i < roles.length; i++) {
     let role = roles[i].title;
@@ -35,25 +67,19 @@ function generateRoles(roles) {
 }
 
 // * Whenever the role is changed, the image should be updated with the `img` of that role.
-function updateImage(roles) {
-  let selectedRole = roleChoice.options[roleChoice.selectedIndex].text;
+function changeImageDisplayed(roles) {
+  let roleChosen = roleChoice.options[roleChoice.selectedIndex].text;
 
   for (let i=0; i <roles.length; i++) {
-    if (selectedRole == roles[i].title) {
+    if (roleChosen == roles[i].title) {
       imageChoice.src = roles[i].img;
     }
   }
   return;
 }
 
-// * Whenever the save button is clicked, *the default behavior should be prevented*, and a POST request should be made to the following API endpoint:
-//
-//     ```
-//     https://galvanize-student-apis.herokuapp.com/gpersonnel/users
-//     ```
 
 // * The format of the body for the POST request should be:
-//
 //   ```js
 //   {
 //     firstName: 'First',
@@ -61,5 +87,22 @@ function updateImage(roles) {
 //     role: 'Selected Role'
 //   }
 //   ```
+function covertFormDataToObj() {
+  let firstName = document.querySelector('#first_name').value;
+  let lastName = document.querySelector('#last_name').value;
+  let role = document.querySelector('#role').value;
+
+  if (firstName.length === 0  || lastName.length === 0) {
+    return;
+  }
+
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    role: role
+  };
+}
+
+
 
 // * No key or value in the POST body can be empty and the role must match one of the roles returned from the initial API call.
